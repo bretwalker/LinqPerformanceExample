77febf2fe2d11c5776b91f9bce02d7e4e8caaaf8
unknown <BWalker@in5309vm.ET.LOCAL>
2014-05-16 14:32:29 -0400

    Added a looping example

--- LinqPerformanceExample/Program.cs
+++ LinqPerformanceExample/Program.cs
@@ -8,17 +8,25 @@ namespace LinqPerformanceExample
     class Program
     {
         private const int Loops = 10000;
-        private const int Iterations = 2;
+        private const int Iterations = 10;
+        private const int ItemsToMap = 100;
 
         private static void Main(string[] args)
         {
             var linqRuntimes = new List<double>();
             var dictRuntimes = new List<double>();
 
-            var people = new List<Person>();
-            for (var i = 0; i < 1000; i++)
+            // Internal objects have no value. They will need to be mapped.
+            var internalObjects = new List<InternalObject>();
+            for (var i = 0; i < ItemsToMap; i++)
             {
-                people.Add(new Person(i.ToString()));
+                internalObjects.Add(new InternalObject {Key = i.ToString()});
+            }
+
+            var externalObjects = new List<ExternalObject>();
+            for (var i = 0; i < ItemsToMap; i++)
+            {
+                externalObjects.Add(new ExternalObject {Key = i.ToString(), Value = i.ToString()});
             }
 
             var stopwatch = new Stopwatch();
@@ -26,18 +34,18 @@ namespace LinqPerformanceExample
             for (var i = 0; i < Iterations; i++)
             {
                 var random = new Random();
-                people = people.OrderBy(p => random.Next()).ToList();
+                internalObjects = internalObjects.OrderBy(p => random.Next()).ToList();
+                externalObjects = externalObjects.OrderBy(p => random.Next()).ToList();
 
                 // Linq
                 stopwatch.Reset();
                 stopwatch.Start();
                 for (var l = 0; l < Loops; l++)
                 {
-                    // Each call is O(n)
-                    // And it's common to see multiple Linq
-                    // statements looking through a List in a single method
-                    var person1 = people.Single(p => p.Name == "1");
-                    var person2 = people.Single(p => p.Name == "2");
+                    foreach (var internalObject in internalObjects)
+                    {
+                        internalObject.Value = externalObjects.First(o => o.Key == internalObject.Key).Value;
+                    }
                 }
                 stopwatch.Stop();
                 linqRuntimes.Add(stopwatch.Elapsed.TotalMilliseconds);
@@ -45,33 +53,36 @@ namespace LinqPerformanceExample
                 // Dictionary
                 stopwatch.Reset();
                 stopwatch.Start();
-                var peopleDict = people.ToDictionary(p => p.Name, p => p);
+                var externalObjectDict = externalObjects.ToDictionary(o => o.Key, o => o);
                 for (var l = 0; l < Loops; l++)
                 {
-                    // Each call is O(1)
-                    var person1 = peopleDict["1"];
-                    var person2 = peopleDict["2"];
+                    foreach (var internalObject in internalObjects)
+                    {
+                        internalObject.Value = externalObjectDict[internalObject.Key].Value;
+                    }
                 }
                 stopwatch.Stop();
                 dictRuntimes.Add(stopwatch.Elapsed.TotalMilliseconds);
             }
 
             Console.Write("Linq average:\t");
-            Console.WriteLine(linqRuntimes.Average()); // 800.916
+            Console.WriteLine(linqRuntimes.Average()); // 1193.32
             Console.Write("Dict average:\t");
-            Console.WriteLine(dictRuntimes.Average()); // 0.8859 
+            Console.WriteLine(dictRuntimes.Average()); // 52.10
             Console.WriteLine();
             Console.ReadKey();
         }
     }
 
-    internal class Person
+    internal class InternalObject
     {
-        public string Name { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
 
-        public Person(string name)
-        {
-            Name = name;
-        }
+    internal class ExternalObject
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
